3d1419374956e1ad3a6415e80c1919efb537b7f1
kvoid <kianheitkamp@outlook.com>
2023-09-11 22:09:04 +0200

    basic setup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,11 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![allow(non_snake_case)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+mod convert;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
